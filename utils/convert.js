9270e924a78429d133d36a8810149dcf32a4af40
SPGoding <SPGoding@outlook.com>
2019-10-07 01:07:32 +0800

    :boom: Support JSON schema draft 2019-09
    Resolve #17
    
    This introduces the following breaking change:
    
    - All `definitions` keys in JSON schemas are now renamed to `$defs`.

--- utils/convert.js
+++ utils/convert.js
@@ -13,7 +13,7 @@ const registriesJson = JSON.parse(fs.readFileSync(join(__dirname, './registries.
 
 function convert(keyName, fileName) {
     const ans = {
-        $schema: 'http://json-schema.org/draft-07/schema',
+        $schema: 'https://json-schema.org/draft/2019-09/schema',
         $id: `https://raw.githubusercontent.com/SPGoding/datapack-json/master/src/shared/${fileName}.json`,
         type: 'string',
         enum: []
